f4dc61ffc75606e5be6a9d7c3273110fc26212bc
Naman Jain <namanjain01508@gmail.com>
2026-05-10 08:00:29 +0530

    feat: setup Google OAuth and Prisma Adapter for next-auth

--- auth.ts
+++ auth.ts
@@ -1,8 +1,16 @@
 import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
+import Google from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/server/trpc"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     Credentials({
       name: "Phone",
       credentials: {
